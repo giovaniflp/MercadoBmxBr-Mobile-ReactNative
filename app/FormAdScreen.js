2cafe8f648883121374782bb90d58441acc926b4
giovaniflp <giovanifeitosa123@gmail.com>
2024-03-04 09:49:59 -0300

    Usando react-native-picker para seletor de categorias

--- app/FormAdScreen.js
+++ app/FormAdScreen.js
@@ -1,13 +1,22 @@
 import { View, Text } from "react-native";
+import { useState } from "react/cjs/react.development";
 
 import BottomBar from "./components/BottomBar";
+import {Picker} from '@react-native-picker/picker';
 import { Link } from "expo-router"
 
 export default function FormAdScreen(){
+
+    const [selectedCategory, setSelectedCategory] = useState();
+
     return(
         <View className="h-full mt-8">
-            <View>
-                <Text className="p-4">Faça aqui seu anúncio</Text>
+            <View className="border-2 m-4 rounded-lg">
+                <Picker selectedValue={selectedCategory} onValueChange={(itemValue, itemIndex)=> setSelectedCategory(itemValue)}>
+                    <Picker.Item label="Selecione uma categoria"></Picker.Item>
+                    <Picker.Item label="Guidão" value="guidao"></Picker.Item>
+                    <Picker.Item label="Quadro" value="quadro"></Picker.Item>
+                </Picker>
             </View>
             <BottomBar></BottomBar>
         </View>
