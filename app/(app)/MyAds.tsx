a01d9f03bc1a48806ff6010461a0b4996674d7a5
giovaniflp <giovanifeitosa123@gmail.com>
2024-04-23 15:15:59 -0300

    Deletando o anúncio do banco e da Azure, corrigir que: apaga o anúncio mas ele não sai da tela

--- app/(app)/MyAds.tsx
+++ app/(app)/MyAds.tsx
@@ -1,8 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import BottomBar from "../components/BottomBar";
 import * as SecureStore from 'expo-secure-store';
 import { useState, useEffect } from "react";
 import axiosInstance from "../server/axios";
+import {router} from "expo-router";
 
 export default function MyAds(){
 
@@ -12,14 +13,12 @@ export default function MyAds(){
     const jwtDecode = async () => {
         try{
             const token = await SecureStore.getItemAsync('session');
-            console.log(token);
             const config = {
                 headers: {
                     Authorization: "Bearer " + token
                 }
             }
             const response = await axiosInstance.get("/api/token/jwtDecode", config)
-            console.log(response.data);
             setDecodeJwt(response.data.jti);
         } catch (error) {
             console.log(error);
@@ -50,13 +49,57 @@ export default function MyAds(){
         getUserAds();
     }, [decodeJwt])
 
+    const deleteAd = async (id) => {
+        try{
+            const token = await SecureStore.getItemAsync('session');
+            const config = {
+                headers: {
+                    Authorization: "Bearer " + token
+                }
+            }
+            await axiosInstance.delete("/api/advertisements/delete/" + id, config)
+            getUserAds();
+            alert("Anúncio deletado com sucesso!")
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return(
         <View className="flex h-full">
             <View className="mt-12">
                 <Text>Meus anúncios</Text>
                 <View>
                     {ads.map((ad, index) => {
-                        return <Text className="bg-blue-500 m-1 p-2" key={index}>{ad.categoria} {ad.marca}</Text>
+                        return (
+                            <TouchableOpacity key={index} onPress={()=>{
+                                router.push({
+                                    pathname: "/FullAdScreen/[id]",
+                                    params: {
+                                        id: ad.id
+                                    }
+                                })
+                            }}>
+                                <View className="bg-blue-500 m-1 p-2 flex flex-row justify-between items-center">
+                                    <View className="flex flex-row">
+                                        <View>
+                                            <Image style={{resizeMode:"contain"}} source={{uri:ad.imagem}} className="w-20 h-20"></Image>
+                                        </View>
+                                        <View>
+                                            <Text>{ad.categoria} {ad.marca}</Text>
+                                            <Text>R$ {ad.preco}</Text>
+                                            <Text>{ad.dataPostagem}</Text>
+                                            <Text>Número de contato: {ad.whatsapp}</Text>
+                                        </View>
+                                        
+                                    </View>
+                                    <View>
+                                        <TouchableOpacity onPress={()=>deleteAd(ad.id)}><Image className="w-10 h-10" source={require('../../public/icons/deletePng.png')}></Image></TouchableOpacity>
+                                        <TouchableOpacity><Image className="w-10 h-10" source={require('../../public/icons/editPng.png')}></Image></TouchableOpacity>
+                                    </View>
+                                </View>
+                            </TouchableOpacity>
+                    )
                     })}
                 </View>
             </View>
--- app/LostPassword.tsx
+++ app/LostPassword.tsx
@@ -1,8 +1,23 @@
 import { ImageBackground, View, Text, TextInput, TouchableOpacity } from "react-native";
 import Button from "./components/Button";
-import { Link } from "expo-router";
+import { router } from "expo-router";
+import { useState } from "react";
+import axiosInstance from "./server/axios";
 
 export default function LostPassword(){
+
+    const[email, setEmail] = useState("")
+
+    const sendEmailCode = async() =>{
+        const response = await axiosInstance.post("/api/users/lostPassword", {email: email})
+        if(response.status == 200){
+            alert("Verifique seu E-mail!")
+            router.push("/LoginScreen")
+        } else {
+            alert("Erro ao enviar email!")
+        }
+    }
+
     return(
         <ImageBackground source={require('../public/images/brandWPP.jpg')}>
             <View className="flex justify-center items-center h-full ">
@@ -12,15 +27,13 @@ export default function LostPassword(){
                     </Text>
                     <View className="flex w-60 mt-4 mb-4">
                         <Text className="text-white">Email</Text>
-                        <TextInput className="border-2 border-white p-1 rounded-lg text-white" placeholder="Insira aqui seu email"></TextInput>
+                        <TextInput value={email} onChangeText={setEmail} className="border-2 border-white p-1 rounded-lg text-white" placeholder="Insira aqui seu email"></TextInput>
                     </View>
                     <View className="flex gap-4 items-center">
                         <View>
-                            <Link href="/EmailCode" asChild>
-                                <TouchableOpacity className="bg-green-400 p-3 rounded-lg w-40">
-                                    <Text className="text-white text-center">Enviar</Text>
-                                </TouchableOpacity>
-                            </Link>
+                            <TouchableOpacity onPress={sendEmailCode} className="bg-green-400 p-3 rounded-lg w-40">
+                                <Text className="text-white text-center">Enviar</Text>
+                            </TouchableOpacity>
                         </View>
                         <View>
                             <Button text="Tela Principal" route="/" color=""></Button>
