63570d4a75fa32bc645a27b19029ba122f77a462
giovaniflp <giovanifeitosa123@gmail.com>
2024-06-19 17:07:05 -0300

    corrigindo build v2

--- app/server/axios.ts
+++ app/server/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://4.228.159.149:8080",
+    baseURL: process.env.EXPO_PUBLIC_BACKEND_URL,
     });
     
 export default axiosInstance;
